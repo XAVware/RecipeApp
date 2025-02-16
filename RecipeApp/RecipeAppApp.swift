dff30a98c0b687f95fb0f9010ca0f348b7e77801
Ryan Smetana <ryan.smetana@xavware.com>
2025-02-16 18:42:58 -0500

    [Feature] Create data models, Add sample recipe to list

--- RecipeApp/RecipeAppApp.swift
+++ RecipeApp/RecipeAppApp.swift
@@ -5,13 +5,44 @@
 //  Created by Ryan Smetana on 2/16/25.
 //
 
+/*
+ Sample response
+ 
+ {
+     "recipes": [
+         {
+             "cuisine": "British",
+             "name": "Bakewell Tart",
+             "photo_url_large": "https://some.url/large.jpg",           OPTIONAL
+             "photo_url_small": "https://some.url/small.jpg",           OPTIONAL
+             "uuid": "eed6005f-f8c8-451f-98d0-4088e2b40eb6",
+             "source_url": "https://some.url/index.html",               OPTIONAL
+             "youtube_url": "https://www.youtube.com/watch?v=some.id"   OPTIONAL
+         },
+         ...
+     ]
+ }
+ 
+ 
+ Project plan:
+ - Start with the model, make codable and ensure this sample JSON decodes properly
+ - NetworkService for fetching JSON
+    - Might need to account for malformed or empty data
+ - Possibly another service to fetch the image data from URL - this would keep the image conversion logic separate from the JSON fetching logic
+ - CachingService to cache Image (or data) from URL.
+ 
+ */
+
+
+
+
 import SwiftUI
 
 @main
 struct RecipeAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ RecipeApp/ViewModels/HomeViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  HomeViewModel.swift
+//  RecipeApp
+//
+//  Created by Ryan Smetana on 2/16/25.
+//
+
+import SwiftUI
+
+/*
+ 
+ Convert URL to image
+ */
+
+@Observable
+class HomeViewModel {
+    
+    var text = """
+    {
+        "recipes": [
+            {
+                "cuisine": "Malaysian",
+                "name": "Apam Balik",
+                "photo_url_large": "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg",
+                "photo_url_small": "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/small.jpg",
+                "source_url": "https://www.nyonyacooking.com/recipes/apam-balik~SJ5WuvsDf9WQ",
+                "uuid": "0c6ca6e7-e32a-4053-b824-1dbf749910d8",
+                "youtube_url": "https://www.youtube.com/watch?v=6R8ffRRJcrg"
+            }
+        ]
+    }
+    """
+
+    var recipes: [Recipe] = []
+    
+    init() {
+        getRecipes()
+    }
+    
+    func getRecipes() /*-> [Recipe]*/ {
+        let jsonDecoder = JSONDecoder()
+        
+        guard let jsonData = text.data(using: .utf8) else {
+            print("Error converting data")
+            return
+        }
+        
+        do {
+            let recipeResponse = try jsonDecoder.decode(Response.self, from: jsonData) 
+            print(recipeResponse)
+            self.recipes.append(contentsOf: recipeResponse.recipes)
+        } catch {
+            print("Error decoding JSON: \(error)")
+        }
+    }
+    
+    
+}
